63930951d80aa62c4e9d5cd80c2838c84b3158db
Nick Stetich <nstetich@gmail.com>
2016-09-01 13:13:13 -0700

    Listen on `PORT` and return 200

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 var bot = require('./lib/bot.js');
+var express = require('express');
+var app = express();
+
+// We need to return a success code so AppFog doesn't shut us down
+app.get('/', function (req, res) {
+  res.status(200).send();
+});
+
+var server = app.listen(process.env.PORT || 3000, function () {
+  var addr = server.address();
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+});
 
 bot.run();
