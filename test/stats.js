59ebe16f6c5115947167f59c7c80c139bfd32641
Nick Stetich <nstetich@gmail.com>
2016-09-02 22:04:40 -0700

    Rename to stats

--- test/stats.js
+++ test/stats.js
@@ -1,18 +1,18 @@
 /* eslint max-nested-callbacks: ["warn", 5] */
 
 var assert = require('assert');
-var leaderboard = require('../lib/leaderboard');
+var stats = require('../lib/stats');
 
-describe('leaderboard', function () {
+describe('stats', function () {
   describe('log', function () {
     it('should record an infraction for a user', function () {
-      return leaderboard.log('barney', 'buffaloclub', 'punch a puppy');
+      return stats.log('barney', 'buffaloclub', 'punch a puppy');
     });
   });
 
   describe('topUsers', function () {
     it('should return a list of top users', function () {
-      return leaderboard.topUsers().then(function (userList) {
+      return stats.topUsers().then(function (userList) {
         assert.ok(userList.find(function (user) {
           return user.username === 'barney';
         }));
